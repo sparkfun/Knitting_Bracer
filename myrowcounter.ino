aba4a6ab22b5abe13ceed370d4c29a7475ed4b15
Liz <elizabeth.neuhalfen@sparkfun.com>
2015-01-12 14:45:29 -0700

    Update myrowcounter.ino
    used circuit 5 from sik kit to set up button inputs and an led output.

--- myrowcounter.ino
+++ myrowcounter.ino
@@ -1,9 +1,50 @@
 #include <MicroView.h>
 
-voidsetup(){
+const int button1Pin = 2;  // pushbutton 1 pin
+const int button2Pin = 3;  // pushbutton 2 pin
+const int ledPin =  13;    // LED pin
 
+voidsetup(){
+// Set up the pushbutton pins to be an input:
+  pinMode(button1Pin, INPUT);
+  pinMode(button2Pin, INPUT);
+
+  // Set up the LED pin to be an output:
+  pinMode(ledPin, OUTPUT);      
 }
 
 voidloop(){
 
+int button1State, button2State;  // variables to hold the pushbutton states
+
+  // Since a pushbutton has only two states (pushed or not pushed),
+  // we've run them into digital inputs. To read an input, we'll
+  // use the digitalRead() function. This function takes one
+  // parameter, the pin number, and returns either HIGH (5V)
+  // or LOW (GND).
+
+  // Here we'll read the current pushbutton states into
+  // two variables:
+
+  button1State = digitalRead(button1Pin);
+  button2State = digitalRead(button2Pin);
+  
+   if (button1State == LOW)  // if we're pushing button 1
+
+  {
+    digitalWrite(ledPin, HIGH);  // turn the LED on
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);  // turn the LED off
+  }
+  
+    if (button2State == LOW)  // if we're pushing button 2
+
+  {
+    digitalWrite(ledPin, HIGH);  // turn the LED on
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);  // turn the LED off
 }
