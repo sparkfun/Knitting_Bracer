9e017c41b297adee6b34ea848f75cd14e089d424
Liz <elizabeth.neuhalfen@sparkfun.com>
2015-01-14 13:38:02 -0700

    Update myrowcounter.ino
    added from Losthaven: button states,

--- myrowcounter.ino
+++ myrowcounter.ino
@@ -1,7 +1,7 @@
-#include <MicroView.h>
+#include <LiquidCrystal.h>
 
-const int button1Pin = 2;  // pushbutton 1 pin
-const int button2Pin = 3;  // pushbutton 2 pin
+const int button1Pin = A1;  // pushbutton 1 pin
+const int button2Pin = A2;  // pushbutton 2 pin
 const int ledPin =  13;    // LED pin
 
 voidsetup(){
@@ -52,4 +52,52 @@ int button1State, button2State;  // variables to hold the pushbutton states
   else
   {
     digitalWrite(ledPin, LOW);  // turn the LED off
+}
+
+
+/**
+* Button processing. Ideally button should take care of powering up/down, resetting and increasing the counter value.
+*/
+void _checkButtonState() {
+  byte buttonState = digitalRead(BUTTON_PIN);
+
+  if( buttonState == HIGH ) { // Button is pressed
+    if( buttonLastState != buttonState ) { // This is the start of "pressed" condition, store the time of event
+      buttonDownTimestamp = millis();
+    } else { // Button is pressed for some time now
+      if(powerMode == PMODE_NORMAL) {
+        buttonDownDuration = millis() - buttonDownTimestamp;
+      
+        if(buttonDownDuration >= TIME_UNTIL_POWER_CYCLE*1000) { // Super-looooong press, go to stand-by while the button is still pressed
+          if(powerState == ON) {
+              powerState = OFF;
+          } else {
+            powerState = ON;
+          }
+          
+          powerMode = PMODE_SWITCHING;
+          buttonDownDuration = buttonDownTimestamp = 0;
+          return; 
+        } else if(powerState == ON && buttonDownDuration >= TIME_UNTIL_RESET*1000) { // Long press, reset the counter
+          counter = 0;
+          _setValue(counter);
+        }
+      }
+    }
+  } else {
+    if( buttonLastState != buttonState ) { // Button is released
+      buttonDownDuration = millis() - buttonDownTimestamp;
+      if( powerState == ON ) {
+        if(buttonDownDuration < TIME_UNTIL_RESET*1000) { // Short press, increase counter value
+          counter++;
+          _setValue(counter);
+        }
+        
+        buttonDownDuration = buttonDownTimestamp = 0;
+      }
+      powerMode = PMODE_NORMAL;
+    }
+  }
+
+  buttonLastState = buttonState;
 }
