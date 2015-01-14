ddb99916b591cd81e54a28b0be800a062bdf3a87
Liz <elizabeth.neuhalfen@sparkfun.com>
2015-01-14 13:47:20 -0700

    Update myrowcounter.ino
    workin workin

--- myrowcounter.ino
+++ myrowcounter.ino
@@ -2,7 +2,9 @@
 
 const int button1Pin = A1;  // pushbutton 1 pin
 const int button2Pin = A2;  // pushbutton 2 pin
-const int ledPin =  13;    // LED pin
+const int lcd =  13;    // LCD pin
+
+int count;
 
 voidsetup(){
 // Set up the pushbutton pins to be an input:
@@ -10,51 +12,20 @@ voidsetup(){
   pinMode(button2Pin, INPUT);
 
   // Set up the LED pin to be an output:
-  pinMode(ledPin, OUTPUT);      
+  pinMode(ledPin, OUTPUT);  
+   
+   lcd.begin(16, 2);
+   lcd.print("Happy Knitting, Liz!");
+   
+_setValue(counter)
 }
 
 voidloop(){
 
-int button1State, button2State;  // variables to hold the pushbutton states
+_checkButtonState();
+_displayValue();
 
-  // Since a pushbutton has only two states (pushed or not pushed),
-  // we've run them into digital inputs. To read an input, we'll
-  // use the digitalRead() function. This function takes one
-  // parameter, the pin number, and returns either HIGH (5V)
-  // or LOW (GND).
-
-  // Here we'll read the current pushbutton states into
-  // two variables:
-
-  button1State = digitalRead(button1Pin);
-  button2State = digitalRead(button2Pin);
-  
-  // A == B means "EQUIVALENT". This is true if both sides are the same.
-  // A && B means "AND". This is true if both sides are true.
-  // A || B means "OR". This is true if either side is true.
-  // !A means "NOT". This makes anything after it the opposite (true or false).
-  
-   if (button1State == LOW)  // if we're pushing button 1
-
-  {
-    digitalWrite(ledPin, HIGH);  // turn the LED on
-  }
-  else
-  {
-    digitalWrite(ledPin, LOW);  // turn the LED off
-  }
-  
-    if (button2State == LOW)  // if we're pushing button 2
-
-  {
-    digitalWrite(ledPin, HIGH);  // turn the LED on
-  }
-  else
-  {
-    digitalWrite(ledPin, LOW);  // turn the LED off
 }
-
-
 /**
 * Button processing. Ideally button should take care of powering up/down, resetting and increasing the counter value.
 */
